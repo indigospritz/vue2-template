ec1d6d78cf09cb6b7f59309166a725d54b6f7df3
christian <cpchristiandelmonte@gmail.com>
2023-01-21 01:50:59 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# pokemonapp-ui
+
+## Scripts
+
+```bash
+  npm run dev # start dev server
+  npm run build # build for production
+  npm run serve # locally preview production build
+```
---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app" class="flex flex-col justify-center items-center bg-slate-700 h-screen">
+    <HelloWorld msg="Hello Vue 2 + Vite" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+export default {
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+
+</style>
